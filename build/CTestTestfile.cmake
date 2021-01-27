ab3acf8e926c28a55c876a76a0674d25027a79f9
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-01-27 22:42:04 +0300

    89 level gui interface

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jcorwin/Desktop/gomoku
+# Build directory: /Users/jcorwin/Desktop/gomoku/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runUnitTests "runUnitTests")
+set_tests_properties(runUnitTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/jcorwin/Desktop/gomoku/CMakeLists.txt;109;add_test;/Users/jcorwin/Desktop/gomoku/CMakeLists.txt;0;")
